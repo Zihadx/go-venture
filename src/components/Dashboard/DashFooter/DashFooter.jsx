1b4c8d136a1695a9a8e276b023b604be7535fd3a
zihadx <blankzihad@gmail.com>
2025-01-05 22:36:05 +0600

    made dahsboard footer and use dynamic date

--- src/components/Dashboard/DashFooter/DashFooter.jsx
+++ src/components/Dashboard/DashFooter/DashFooter.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const DashFooter = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
         <div className='text-center border-t p-4 mt-10'>
-            <h1>This is Dashboard Footer</h1>
+            <h1>Copyright © {currentYear}. All rights reserved.</h1>
         </div>
     );
 };
